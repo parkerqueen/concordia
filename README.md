cbbace6d8a7a5f4a7c7d1302d802cfa5e9dc32d9
parkerqueen <sobanr4@gmail.com>
2021-10-19 00:20:53 +0500

    added a sample

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# concordia
+# concordia (WIP)
 Raytracer???
+
+# A rendered sample (1920x1080)
+![](images/image.png)
--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,6 @@ Color3 ray_color(const Ray& ray, const Hittable& world, const uint depth) {
 
     const auto unit_direction = ray.direction.unit_vector();
     const auto t = 0.5 * (unit_direction.y + 1.0);
-
     return (1.0 - t) * Color3{1.0, 1.0, 1.0} + t * Color3{0.5, 0.7, 1.0};
 }
 
@@ -27,8 +26,8 @@ int main() {
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
-    const int max_depth = 50;
     const int samples_per_pixel = 100;
+    const int max_shadow_ray_recursion = 50;
 
     const Point3 lookfrom(0.0, 0.0, 1.0);
     const Point3 lookat(0.0, 0.0, -1.0);
@@ -59,7 +58,7 @@ int main() {
                 const auto u = static_cast<double>(i + random_double()) / (image_width - 1);
                 const auto v = static_cast<double>(j + random_double()) / (image_height - 1);
                 const auto r = camera.get_ray(u, v);
-                pixel_color += ray_color(r, world, max_depth);
+                pixel_color += ray_color(r, world, max_shadow_ray_recursion);
             }
 
             ppm_writer.write_color(pixel_color, samples_per_pixel);
